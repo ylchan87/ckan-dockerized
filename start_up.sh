22a3ade30fac1d8da3a35cde331f3d18e87d6e12
yulapshun <yulapshun@gmail.com>
2017-11-26 12:45:39 +0800

    Commit 2

--- start_up.sh
+++ start_up.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
-service postgresql start
+# service postgresql start
 service jetty8 start
 
-echo "start"
-sleep 20
-echo "end"
+sleep 10
 
-if [[ $(sudo -u postgres psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='USR_NAME'") ]]
+if [[ $(sudo -u postgres psql -h postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='USR_NAME'") ]]
 then
-    echo "Shell script is hard"
+    echo ""
 else
-    echo "true"
-    sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD '12345678';"
+    # echo "true"
+    # sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'example';"
 
-    sudo -u postgres psql -c "UPDATE pg_database SET datistemplate = FALSE WHERE datname = 'template1';"
-    sudo -u postgres psql -c "DROP DATABASE template1;"
-    sudo -u postgres psql -c "CREATE DATABASE template1 WITH TEMPLATE = template0 ENCODING = 'UNICODE';"
-    sudo -u postgres psql -c "UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template1';"
-    sudo -u postgres psql -c "\c template1"
-    sudo -u postgres psql -c "VACUUM FREEZE;"
+    # sudo -u postgres psql -c "UPDATE pg_database SET datistemplate = FALSE WHERE datname = 'template1';"
+    # sudo -u postgres psql -c "DROP DATABASE template1;"
+    # sudo -u postgres psql -c "CREATE DATABASE template1 WITH TEMPLATE = template0 ENCODING = 'UNICODE';"
+    # sudo -u postgres psql -c "UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template1';"
+    # sudo -u postgres psql -c "\c template1"
+    # sudo -u postgres psql -c "VACUUM FREEZE;"
 
-    sudo -u postgres createdb -O ckan_default ckan_default -E utf-8
+    # sudo -u postgres createdb -O ckan_default ckan_default -E utf-8
 
     ckan db init
 fi
-
-echo "hihi"
 
 ckan serve
